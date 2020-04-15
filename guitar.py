0602e6b93d2f26e5e6907eabf8b78b0d65ae2da3
marcin-napolski <marcin.napolski@gmail.com>
2020-04-15 16:02:03 +0200

    vocabluary mistake

--- guitar.py
+++ guitar.py
@@ -1,6 +1,6 @@
 from random import randrange
 
-# data related to guitar notes - initally just 0 to 11th thread
+# data related to guitar notes - initally just 0 to 11th fret
 string1 = ["E","F","F#","G","G#","A","A#","B","C","C#","D","D#"]
 string2 = ["B","C","C#","D","D#","E","F","F#","G","G#","A","A#",]
 string3 = ["G","G#","A","A#","B","C","C#","D","D#","E","F","F#",]
@@ -10,14 +10,14 @@ string6 = ["E","F","F#","G","G#","A","A#","B","C","C#","D","D#"]
 notes = [string1,string2,string3,string4,string5,string6]			# 2D array
 
 stringNumber = randrange(6)
-threadNumber = randrange(12)
+fretNumber = randrange(12)
 
 # let's make it easier - test without #-notes 
-while (len(str(notes[stringNumber][threadNumber]))) == 2:
+while (len(str(notes[stringNumber][fretNumber]))) == 2:
         stringNumber = randrange(6)
-        threadNumber = randrange(12)
+        fretNumber = randrange(12)
 
-print("string: " + str(stringNumber+1)+ ", thread: " +  str(threadNumber))
+print("string: " + str(stringNumber+1)+ ", fret: " +  str(fretNumber))
 
 # printing the strings - just to visualize the position
 i = 0
@@ -25,8 +25,8 @@ while i < stringNumber:
 	print ("|---------------|")
 	i += 1
 if i == stringNumber:
-	if threadNumber< 10: print ("|------ " + str(threadNumber)+ " ------|")
-	else: print ("|----- " + str(threadNumber)+ " ------|")			# it's just formatng
+	if fretNumber< 10: print ("|------ " + str(fretNumber)+ " ------|")
+	else: print ("|----- " + str(fretNumber)+ " ------|")			# it's just formatng
 	i += 1
 while i < 6:
 	print ("|---------------|")
@@ -35,5 +35,5 @@ while i < 6:
 answer = input()
 
 # validating the answer
-if str(answer).capitalize()  == str(notes[stringNumber][threadNumber]): print ("Correct!")
-else: print ("Wrong! It's: " + str(notes[stringNumber][threadNumber]))
+if str(answer).capitalize()  == str(notes[stringNumber][fretNumber]): print ("Correct!")
+else: print ("Wrong! It's: " + str(notes[stringNumber][fretNumber]))
